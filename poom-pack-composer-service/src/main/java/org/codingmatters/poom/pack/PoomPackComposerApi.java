c96bc3c403b1f88fbf8e38b0901667e5ba97cd6d
nicolas@flexio.fr <nicolas@flexio.fr>
2018-09-05 11:28:37 +0200

    add delete handlers

--- poom-pack-composer-service/src/main/java/org/codingmatters/poom/pack/PoomPackComposerApi.java
+++ poom-pack-composer-service/src/main/java/org/codingmatters/poom/pack/PoomPackComposerApi.java
@@ -2,6 +2,7 @@ package org.codingmatters.poom.pack;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import org.codingmatters.poom.pack.api.PoomPackComposerHandlers;
+import org.codingmatters.poom.pack.handler.DeleteArtifact;
 import org.codingmatters.poom.pack.handler.GetArtifact;
 import org.codingmatters.poom.pack.handler.GetPackage;
 import org.codingmatters.poom.pack.handler.SavePackage;
@@ -19,6 +20,7 @@ public class PoomPackComposerApi {
         this.handlers = new PoomPackComposerHandlers.Builder()
                 .packagesGetHandler( new GetPackage( repositoryPath, serviceUrl ) )
                 .repositoryPostHandler( new SavePackage( repositoryPath, api_key ) )
+                .artifactsDeleteHandler( new DeleteArtifact( repositoryPath, api_key ) )
                 .artifactsGetHandler( new GetArtifact( repositoryPath ) )
                 .build();
         this.processor = new PoomPackComposerProcessor( this.path(), new JsonFactory(), handlers );
---
+++ poom-pack-composer-service/src/test/java/org/codingmatters/poom/pack/DeleteArtifactTest.java
@@ -0,0 +1,99 @@
+package org.codingmatters.poom.pack;
+
+import org.codingmatters.poom.pack.api.ArtifactsDeleteRequest;
+import org.codingmatters.poom.pack.api.RepositoryPostRequest;
+import org.codingmatters.poom.pack.handler.DeleteArtifact;
+import org.codingmatters.poom.pack.handler.SavePackage;
+import org.codingmatters.rest.api.types.File;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DeleteArtifactTest {
+
+    private final String API_KEY = "9";
+
+    private TemporaryFolder temp;
+    private SavePackage savePackage;
+    private DeleteArtifact deleteArtifact;
+    private String vendor;
+    private String artifactId;
+    private String artifactVersion;
+
+    @Before
+    public void setUp() throws Exception {
+        temp = new TemporaryFolder();
+        temp.create();
+        deleteArtifact = new DeleteArtifact( temp.getRoot().getPath(), API_KEY );
+        savePackage = new SavePackage( temp.getRoot().getPath(), API_KEY );
+
+        Path zipArchive = Paths.get( Thread.currentThread().getContextClassLoader().getResource( "flexio-tabular-php-client-1.0.0-SNAPSHOT.zip" ).getPath() );
+        byte[] content = Files.readAllBytes( zipArchive );
+
+        vendor = "flexio-services";
+        artifactId = "flexio-tabular-php-client";
+        artifactVersion = "1.0.0-dev";
+        savePackage.apply(
+                RepositoryPostRequest.builder()
+                        .xVendor( vendor )
+                        .xArtifactId( artifactId )
+                        .xVersion( artifactVersion )
+                        .xApiKey( API_KEY )
+                        .payload( File.builder()
+                                .content( content )
+                                .contentType( "application/zip" )
+                                .build() )
+                        .build()
+        ).opt().status201().orElseThrow( ()->new AssertionError( "Cannot save the artifact" ) );
+    }
+
+    @Test
+    public void testRemoveWithBadApiKey_thenReturn403() {
+        deleteArtifact.apply(
+                ArtifactsDeleteRequest.builder()
+                        .xApiKey( "BAD_API_KEY" )
+                        .vendor( vendor )
+                        .packageName( artifactId )
+                        .version( artifactVersion )
+                        .fileName( artifactId + "-" + artifactVersion + ".zip" )
+                        .build()
+        ).opt().status403().orElseThrow( ()->new AssertionError( "Should get 403 with bad api key" ) );
+    }
+
+    @Test
+    public void testRemove_thenReturn204_thenFileDoNotExist() {
+        String filePath = temp.getRoot().getPath() + "/" + vendor + "/" + artifactId + "/" + artifactVersion + "/" + artifactId + "-" + artifactVersion + ".zip";
+        assertTrue( new java.io.File( filePath ).exists() );
+        deleteArtifact.apply(
+                ArtifactsDeleteRequest.builder()
+                        .xApiKey( API_KEY )
+                        .vendor( vendor )
+                        .packageName( artifactId )
+                        .version( artifactVersion )
+                        .fileName( artifactId + "-" + artifactVersion + ".zip" )
+                        .build()
+        ).opt().status204().orElseThrow( ()->new AssertionError( "Should delete this artifact" ) );
+        assertFalse( new java.io.File( filePath ).exists() );
+    }
+
+    @Test
+    public void testRemoveNonexistentArtifact_thenReturn404() {
+        deleteArtifact.apply(
+                ArtifactsDeleteRequest.builder()
+                        .xApiKey( API_KEY )
+                        .vendor( vendor )
+                        .packageName( artifactId )
+                        .vendor( artifactVersion )
+                        .fileName( artifactId + "-" + artifactVersion + ".zip" )
+                        .build()
+        ).opt().status404().orElseThrow( ()->new AssertionError( "Should get 404 with non existent artifact" ) );
+
+    }
+}
